943dde6b6f6e02c5582bac8eff3f437bc7e69196
Gabriel Pinese <gabriel.pinese@gmail.com>
2022-08-04 15:54:41 -0300

    exemplo sobreposicao usando @Override com o Super no metodo

---
+++ src/heranca/exemplos/supers/MainFinal.java
@@ -0,0 +1,9 @@
+package heranca.exemplos.supers;
+
+public class MainFinal {
+    /* a par final impede que que a classe seja extendida
+    que os metodos sejam sobrepostos, é orientado que metodos sobrepostos sejam final
+    pois sobreposicoes em metod sobrepostos podem gerer inconsistencias
+    a palavra final tbm melhora a performance.
+     */
+}
