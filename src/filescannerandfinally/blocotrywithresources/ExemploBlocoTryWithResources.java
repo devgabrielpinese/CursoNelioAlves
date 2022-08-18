c920d575c1e2edc4297a87952184bd38c406d17d
Gabriel Pinese <gabriel.pinese@gmail.com>
2022-08-18 03:35:09 -0300

    Bloco try-with-resources instacia uma variavel BufferadReder direto dentro do bloco try usa uma nova instacia de FileReader como argumento ou parametro e essa usa uma outra variavel que recebe um arquivo de texto

---
+++ src/filescannerandfinally/blocotrywithresources/ExemploBlocoTryWithResources.java
@@ -0,0 +1,26 @@
+package filescannerandfinally.blocotrywithresources;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ExemploBlocoTryWithResources {
+    public static void main(String[] args) {
+        String path =  "C:\\Users\\gabri\\IdeaProjects\\NelioAlves\\text";
+        //lancamos direto dentro do bloco try com parenteses
+        // um BufferedReader br instaciamos BufferedReader com argumento ou parametro
+        // uma nova instacia de FileReader com argumento ou parametro path
+        //isso em vez de fazer separado como o outro exeplo, mandamos direto
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            //criamos a variavel para ler linha a linha com o while
+            String line = br.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+            // e tratamos a exception
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
