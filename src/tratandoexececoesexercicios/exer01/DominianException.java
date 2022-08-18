fa2a4522623d5c0c483847473dbde706c078727b
Gabriel Pinese <gabriel.pinese@gmail.com>
2022-08-17 21:37:44 -0300

    solucao boa, corrigido e testado. estava usando uma classe de outro pacote que tinha o mesmo nome DominianException, otimo aprendizado para verificar de qual classe estou chamando o metodo

---
+++ src/tratandoexececoesexercicios/exer01/DominianException.java
@@ -0,0 +1,11 @@
+package tratandoexececoesexercicios.exer01;
+
+public class DominianException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public DominianException (String msg){
+        super(msg);
+    }
+
+
+}
